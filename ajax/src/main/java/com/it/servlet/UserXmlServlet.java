aa2f8f9d87e3adaead0b9e187b139f2ca9cb6bac
BIGMANBIN <HB_1026@163.com>
2016-06-21 16:19:36 +0800

    添加了XML

---
+++ ajax/src/main/java/com/it/servlet/UserXmlServlet.java
@@ -0,0 +1,5 @@
+package com.it.servlet;
+
+
+public class UserXmlServlet {
+}
---
+++ web1/src/com/it/web/CommentFileUploadServlet.java
@@ -0,0 +1,7 @@
+package com.it.web;
+
+/**
+ * Created by Administrator on 2016/6/20 0020.
+ */
+public class CommentFileUploadServlet {
+}
