4dfb45e4584ba6f42c26e23eb51f1c26e2651cca
BIGMANBIN <HB_1026@163.com>
2016-06-22 17:42:17 +0800

    修改了爬虫

--- ajax/src/test/java/com/it/test/JSoupTestCase.java
+++ ajax/src/test/java/com/it/test/JSoupTestCase.java
@@ -8,25 +8,22 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.junit.Test;
 
-import java.io.IOException;
-
-
 public class JSoupTestCase {
 
     @Test
-    public void testGetImage() throws IOException {
+    public void testGetImage() throws Exception {
 
 
-            Document document = Jsoup.connect("http://www.topit.me/pop?p=1").cookie("is_click", "1").get();
+            Document document = Jsoup.connect("http://www.topit.me/tag/风景？p=5").cookie("is_click","1").get();
             //找元素
 
             Elements elements = document.select("#content .catalog .e>a");
             //拿到对应大图超链接
             for (Element element : elements) {
                 String href = element.attr("href");//href属性
-                System.out.println("href:" + href);
+                //System.out.println("href:" + href);
                 //请求大图片链接地址
-                Document bigImageDoc = Jsoup.connect(href).cookie("is_click", "1").get();
+                Document bigImageDoc = Jsoup.connect(href).cookie("is_click","1").get();
                 Element imgElement = bigImageDoc.select("#content>a").first();
                 String imgSrc = imgElement.attr("href");
 
@@ -35,12 +32,8 @@ public class JSoupTestCase {
                 String fileName = imgSrc.substring(imgSrc.lastIndexOf("/") + 1);
                 HttpUtil.getRequestStream(imgSrc, "E:/upload/" + fileName);
 
-
+                Thread.sleep(3000);
             }
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+
     }
 }
